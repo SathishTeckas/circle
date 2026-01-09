148fe8747f41fd49c68c5e4852b66629a2bd918b
Monish Muralidharan <monish@teckastechnologies.com>
2026-01-09 10:13:15 +0000

    File changes

--- src/pages/ChatView.jsx
+++ src/pages/ChatView.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { base44 } from '@/api/base44Client';
+import { createPageUrl } from '../utils';
 import { formatTime12Hour } from '../components/utils/timeFormat';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '@/components/ui/button';
